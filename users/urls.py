4aa45e0b4c73f0e94c537a3558fb25513ed11bb7
Petr Maev <petr.maev94@gmail.com>
2025-07-02 23:46:15 +0300

    add urls for CRUD of users

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,25 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 
 from users.apps import UsersConfig
-from users.views import (CustomUserViewSet, PaymentsCreateAPIView, PaymentsDestroyAPIView, PaymentsListAPIView,
-                         PaymentsRetrieveAPIView, PaymentsUpdateAPIView)
+from users.views import (PaymentsCreateAPIView, PaymentsDestroyAPIView, PaymentsListAPIView,
+                         PaymentsRetrieveAPIView, PaymentsUpdateAPIView, CustomUserCreateAPIView, CustomUserListAPIView,
+                         CustomUserRetrieveAPIView, CustomUserUpdateAPIView, CustomUserDestroyAPIView)
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = UsersConfig.name
 
-router = DefaultRouter()
-router.register(r"users", CustomUserViewSet, basename="users")
-
 urlpatterns = [
-                  path("payments/create/", PaymentsCreateAPIView.as_view(), name="payment_create"),
-                  path("payments/", PaymentsListAPIView.as_view(), name="payments_list"),
-                  path("payments/<int:pk>/", PaymentsRetrieveAPIView.as_view(), name="payment_detail"),
-                  path("payments/<int:pk>/edit/", PaymentsUpdateAPIView.as_view(), name="payment_edit"),
-                  path("payments/delete/<int:pk>/", PaymentsDestroyAPIView.as_view(), name="payment_delete"),
-              ] + router.urls
+    path("users/create/", CustomUserCreateAPIView.as_view(), name="user_create"),
+    path("users/", CustomUserListAPIView.as_view(), name="user_list"),
+    path("users/<int:pk>/", CustomUserRetrieveAPIView.as_view(), name="user_detail"),
+    path("users/<int:pk>/edit/", CustomUserUpdateAPIView.as_view(), name="user_edit"),
+    path("users/delete/<int:pk>/", CustomUserDestroyAPIView.as_view(), name="user_delete"),
+    path("payments/create/", PaymentsCreateAPIView.as_view(), name="payment_create"),
+    path("payments/", PaymentsListAPIView.as_view(), name="payments_list"),
+    path("payments/<int:pk>/", PaymentsRetrieveAPIView.as_view(), name="payment_detail"),
+    path("payments/<int:pk>/edit/", PaymentsUpdateAPIView.as_view(), name="payment_edit"),
+    path("payments/delete/<int:pk>/", PaymentsDestroyAPIView.as_view(), name="payment_delete"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
