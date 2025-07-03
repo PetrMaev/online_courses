b1c4667ef33905a7dea36be5664295ef280858ba
Petr Maev <petr.maev94@gmail.com>
2025-07-03 22:16:17 +0300

    add new class permission

--- users/permissions.py
+++ users/permissions.py
@@ -15,3 +15,12 @@ class IsOwner(permissions.BasePermission):
         if obj.owner == request.user:
             return True
         return False
+
+
+class IsUserOwner(permissions.BasePermission):
+    """Проверяет, является ли пользователь владельцем своего профиля."""
+
+    def has_object_permission(self, request, view, obj):
+        if obj.email == request.user.email:
+            return True
+        return False
