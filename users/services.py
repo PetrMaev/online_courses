9f46bf8527609c8f483f510987e532634d1f1f0f
Petr Maev <petr.maev94@gmail.com>
2025-07-23 00:22:36 +0300

    add new function for convert rub in usd

--- users/services.py
+++ users/services.py
@@ -1,5 +1,6 @@
 import stripe
 from config.settings import STRIPE_API_KEY
+from forex_python.converter import CurrencyRates
 from materials.models import Course, Lesson
 
 stripe.api_key = STRIPE_API_KEY
@@ -20,12 +21,19 @@ def create_stripe_lesson_product(lesson_id):
     product = stripe.Product.create(name=lesson.title)
     return product
 
+def convert_rub_to_usd(amount):
+    """Конвертирует рубли в доллары."""
+
+    c = CurrencyRates()
+    rate = c.get_rate("RUB", "USD")
+    return int(amount * rate)
+
 
 def create_stripe_price(amount, product):
     """Создание цены на страйпе."""
 
     price = stripe.Price.create(
-        currency="rub",
+        currency="usd",
         unit_amount=amount * 100,
         product_data={"name": product.get("name")},
     )
@@ -41,3 +49,21 @@ def create_stripe_session(price):
         mode="payment",
     )
     return session.get("id"), session.get("url")
+
+
+def checkout_course_session(course_id, session_id):
+    course = Course.objects.get(id=course_id)
+    session = stripe.checkout.Session.retrieve(session_id)
+    if session.get("payment_status") != "unpaid":
+        course.save(is_paid=True)
+        return session.get("payment_status")
+    return session.get("payment_status")
+
+
+def checkout_lesson_session(lesson_id, session_id):
+    lesson = Lesson.objects.get(id=lesson_id)
+    session = stripe.checkout.Session.retrieve(session_id)
+    if session.get("payment_status") != "unpaid":
+        lesson.save(is_paid=True)
+        return session.get("payment_status")
+    return session.get("payment_status")
