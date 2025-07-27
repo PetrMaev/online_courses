65d7c19f6b988a40ef1224d05d434da7b7a44bf6
Petr Maev <petr.maev94@gmail.com>
2025-07-28 01:22:26 +0300

    fixed function

--- users/tasks.py
+++ users/tasks.py
@@ -6,28 +6,17 @@ from celery import shared_task
 from users.models import CustomUser
 
 
-# @shared_task
-# def check_last_login_user():
-#     users = CustomUser.objects.all()
-#     month_ago = timezone.now() - timedelta(days=30)
-#     for user in users:
-#         if user.groups.filter(name='Moderators').not_exists() or user.filter(
-#                 is_superuser=True).not_exists():
-#             user.filter(last_login__lt=month_ago, is_active=True).update(
-#                 is_active=False
-#         )
-#         else:
-#             continue
-
-
 @shared_task
 def check_last_login_user():
     today = timezone.now().today()
     users = CustomUser.objects.all()
     for user in users:
-        if user.groups.filter(
-                name='Moderators').not_exists() or user.is_superuser != True and today - user.last_login > timedelta(
-                days=30):
+        if (
+            user.is_staff != True
+            or user.is_superuser != True
+            and user.last_login is not None
+            and today - user.last_login > timedelta(days=30)
+        ):
             user.is_active = False
             user.save()
         else:
