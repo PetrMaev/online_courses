4418c3b5387179931bf47a22340e543be3e46285
Petr Maev <petr.maev94@gmail.com>
2025-07-27 22:27:34 +0300

    add task for send email about update course

---
+++ materials/tasks.py
@@ -0,0 +1,12 @@
+from config.settings import EMAIL_HOST_USER
+from celery import shared_task
+from django.core.mail import send_mail
+
+@shared_task
+def send_email_course_update(course_title, email):
+    send_mail(
+        f'Обновления в курсе {course_title}',
+        'Перейдите в личный кабинет, чтобы ознакомиться с обновлениями',
+        EMAIL_HOST_USER,
+        [email]
+    )
