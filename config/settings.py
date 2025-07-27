c5c7224dd26fa354253b8f2338c81163fc56c5c8
Petr Maev <petr.maev94@gmail.com>
2025-07-28 01:21:49 +0300

    edit timedelta in celery_beat settings

--- config/settings.py
+++ config/settings.py
@@ -160,12 +160,10 @@ CELERY_TASK_TRACK_STARTED = True
 # Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
-# CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
-
 CELERY_BEAT_SCHEDULE = {
     'check_last_login_user': {
         'task': 'users.tasks.check_last_login_user',  # Путь к задаче
-        'schedule': timedelta(seconds=10),  # Расписание выполнения задачи
+        'schedule': timedelta(days=30),  # Расписание выполнения задачи
     },
 }
 
