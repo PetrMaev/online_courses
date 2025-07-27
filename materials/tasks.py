51b9c6e3fea8c62cc9471311f05d8a082eec7427
Petr Maev <petr.maev94@gmail.com>
2025-07-28 01:24:22 +0300

    small fixes

--- materials/tasks.py
+++ materials/tasks.py
@@ -2,6 +2,7 @@ from config.settings import EMAIL_HOST_USER
 from celery import shared_task
 from django.core.mail import send_mail
 
+
 @shared_task
 def send_email_course_update(course_title, email):
     send_mail(
