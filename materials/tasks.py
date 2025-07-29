8239c9d98ade31c3ba1c34a5210190e07de4e95b
Petr Maev <petr.maev94@gmail.com>
2025-07-29 22:15:20 +0300

    update task

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,13 +1,17 @@
-from config.settings import EMAIL_HOST_USER
+from config import settings
 from celery import shared_task
 from django.core.mail import send_mail
 
+from materials.models import Course
+
 
 @shared_task
-def send_email_course_update(course_title, email):
+def send_email_course_update(course_id: int) -> None:
+    course = Course.objects.get(id=course_id)
+    recipients = course.subscriptions.values_list('user__email', flat=True)
     send_mail(
-        f'Обновления в курсе {course_title}',
+        f'Обновления в курсе {course.title}',
         'Перейдите в личный кабинет, чтобы ознакомиться с обновлениями',
-        EMAIL_HOST_USER,
-        [email]
+        settings.EMAIL_HOST_USER,
+        recipients
     )
